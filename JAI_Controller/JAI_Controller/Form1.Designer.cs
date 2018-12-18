a3c8eb98a943184fe885a4780c88049a20275be5
Zhou Yong <1508108492@qq.com>
2018-12-18 15:31:59 +0800

    V1.0
    焊缝跟踪器C#版本V1.0

---
+++ JAI_Controller/JAI_Controller/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace JAI_Controller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.gainTrackBar = new System.Windows.Forms.TrackBar();
+            this.exposureTrackBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gainTextBox = new System.Windows.Forms.TextBox();
+            this.exposureTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gainTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exposureTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(19, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(461, 390);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(498, 16);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(478, 389);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(736, 432);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开启";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(738, 508);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 58);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "关闭";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // gainTrackBar
+            // 
+            this.gainTrackBar.Location = new System.Drawing.Point(107, 453);
+            this.gainTrackBar.Name = "gainTrackBar";
+            this.gainTrackBar.Size = new System.Drawing.Size(372, 45);
+            this.gainTrackBar.TabIndex = 4;
+            this.gainTrackBar.Scroll += new System.EventHandler(this.gainTrackBar_Scroll);
+            // 
+            // exposureTrackBar
+            // 
+            this.exposureTrackBar.Location = new System.Drawing.Point(107, 505);
+            this.exposureTrackBar.Name = "exposureTrackBar";
+            this.exposureTrackBar.Size = new System.Drawing.Size(371, 45);
+            this.exposureTrackBar.TabIndex = 5;
+            this.exposureTrackBar.Scroll += new System.EventHandler(this.exposureTrackBar_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 454);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "增益";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 508);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "曝光";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(496, 454);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(491, 510);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 12);
+            this.label4.TabIndex = 9;
+            // 
+            // gainTextBox
+            // 
+            this.gainTextBox.Location = new System.Drawing.Point(494, 452);
+            this.gainTextBox.Name = "gainTextBox";
+            this.gainTextBox.Size = new System.Drawing.Size(100, 21);
+            this.gainTextBox.TabIndex = 10;
+            // 
+            // exposureTextBox
+            // 
+            this.exposureTextBox.Location = new System.Drawing.Point(494, 501);
+            this.exposureTextBox.Name = "exposureTextBox";
+            this.exposureTextBox.Size = new System.Drawing.Size(100, 21);
+            this.exposureTextBox.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 596);
+            this.Controls.Add(this.exposureTextBox);
+            this.Controls.Add(this.gainTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exposureTrackBar);
+            this.Controls.Add(this.gainTrackBar);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gainTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exposureTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar gainTrackBar;
+        private System.Windows.Forms.TrackBar exposureTrackBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox gainTextBox;
+        private System.Windows.Forms.TextBox exposureTextBox;
+    }
+}
+
---
+++ JAI_Controller/JAI_Controller/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Jai_FactoryDotNET;
+using System.Drawing.Imaging;
+
+namespace JAI_Controller
+{
+    public partial class Form1 : Form
+    {
+        static Jai_FactoryDotNET.CFactory factory;
+        static CCamera myCamera;
+
+        CNode myGainNode;
+        CNode myExposureNode;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+            Jai_FactoryWrapper.EFactoryError error = Jai_FactoryWrapper.EFactoryError.Success;
+
+            factory = new Jai_FactoryDotNET.CFactory();
+
+            error = factory.Open("");
+
+            SearchButton_Click(null, null);
+        }
+        /// <summary>
+        /// 开启相机
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(myCamera!=null)
+            {
+                myCamera.NewImageDelegate += new Jai_FactoryWrapper.ImageCallBack(HandleImage);
+                myCamera.StartImageAcquisition(true, 5, pictureBox1.Handle);
+                //myCamera.StartImageAcquisition(true, 5);
+
+                button2.Enabled = true;
+                button1.Enabled = false;
+            }
+        }
+        /// <summary>
+        /// 关闭相机
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (myCamera != null)
+            {
+                myCamera.StopImageAcquisition();
+                myCamera.NewImageDelegate -= new Jai_FactoryWrapper.ImageCallBack(HandleImage);
+            }
+
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            // Search for any new GigE cameras using Filter Driver
+            factory.UpdateCameraList(Jai_FactoryDotNET.CFactory.EDriverType.FilterDriver);
+
+            if (factory.CameraList.Count > 0)
+            {
+                // Open the camera
+                myCamera = factory.CameraList[0];
+                myCamera.Open();
+
+                // Get the GainRaw GenICam Node
+                myGainNode = myCamera.GetNode("GainRaw");
+                if (myGainNode != null)
+                {
+                    gainTrackBar.Minimum = Convert.ToInt32(myGainNode.Min);
+                    gainTrackBar.Maximum = Convert.ToInt32(myGainNode.Max);
+                    //gainTrackBar.Value = Convert.ToInt32(myGainNode.Value);
+                    gainTrackBar.TickFrequency = (gainTrackBar.Maximum - gainTrackBar.Minimum) / 20;
+                    gainTextBox.Text = myGainNode.Value.ToString();                    
+                }
+
+                // Get the Exposure GenICam Node
+                myExposureNode = myCamera.GetNode("ExposureTimeRaw");
+                if (myExposureNode != null)
+                {
+                    exposureTrackBar.Minimum = Convert.ToInt32(myExposureNode.Min);
+                    exposureTrackBar.Maximum = Convert.ToInt32(myExposureNode.Max);
+                    //exposureTrackBar.Value = Convert.ToInt32(myExposureNode.Value);
+                    exposureTrackBar.TickFrequency = (exposureTrackBar.Maximum - exposureTrackBar.Minimum) / 20;
+                    exposureTextBox.Text = myExposureNode.Value.ToString();
+                }
+
+
+                button1.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("No Cameras Found!");
+            }
+        }
+
+        ColorPalette myMonoColorPalette = null;
+        void HandleImage(ref Jai_FactoryWrapper.ImageInfo ImageInfo)
+        {  
+            Bitmap newImageBitmap = new Bitmap((int)ImageInfo.SizeX, (int)ImageInfo.SizeY,  
+                    (int)ImageInfo.SizeX, System.Drawing.Imaging.PixelFormat.Format8bppIndexed,
+                    ImageInfo.ImageBuffer);
+
+            // Create a Monochrome palette (only once)
+            if (myMonoColorPalette == null)
+            {
+                Bitmap monoBitmap = new Bitmap(1, 1, PixelFormat.Format8bppIndexed);
+                myMonoColorPalette = monoBitmap.Palette;
+
+                for (int i = 0; i < 256; i++)
+                    myMonoColorPalette.Entries[i] = Color.FromArgb(i, i, i);
+            }
+
+            for (int i = 0; i < 256; i++)
+                myMonoColorPalette.Entries[i] = Color.FromArgb(i, i, i);
+
+            // Set the Monochrome Color Palette
+            newImageBitmap.Palette = myMonoColorPalette;
+
+            //=====================================================
+            //2018-12-17
+            byte[] imgData;
+            int stride;
+            int ImageChannel = Image.GetPixelFormatSize(newImageBitmap.PixelFormat) / 8;
+            getByteFrmBitmap(newImageBitmap, out imgData, out stride);
+
+            pictureBox2.Image = ToGrayBitmap(imgData, (int)ImageInfo.SizeX, (int)ImageInfo.SizeY);
+            return;
+        }
+
+        //从Bitmap中获取图像数据并存入byte[]数组中
+        private void getByteFrmBitmap(Bitmap bp, out byte[] rgbValues, out int stride)
+        {
+            Rectangle rect = new Rectangle(0, 0, bp.Width, bp.Height);
+            System.Drawing.Imaging.BitmapData bmpData =
+            bp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, bp.PixelFormat);
+
+            stride = bmpData.Stride;
+            // Get the address of the first line.
+            IntPtr ptr = bmpData.Scan0;
+
+            // Declare an array to hold the bytes of the bitmap. 
+            int bytes = Math.Abs(bmpData.Stride) * bp.Height;
+            //byte[] rgbValues = new byte[bytes];
+            rgbValues = new byte[bytes];
+
+            // Copy the RGB values into the array.
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            // Unlock the bits.
+            bp.UnlockBits(bmpData);
+        }
+
+        /// <summary>  
+        /// 将一个字节数组转换为8bit灰度位图  
+        /// </summary>  
+        /// <param name="rawValues">显示字节数组</param>  
+        /// <param name="width">图像宽度</param>  
+        /// <param name="height">图像高度</param>  
+        /// <returns>位图</returns>  
+        public static Bitmap ToGrayBitmap(byte[] rawValues, int width, int height)
+        {
+            //// 申请目标位图的变量，并将其内存区域锁定  
+            Bitmap bmp = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, width, height),
+             ImageLockMode.WriteOnly, PixelFormat.Format8bppIndexed);
+
+            //// 获取图像参数  
+            int stride = bmpData.Stride;  // 扫描线的宽度  
+            int offset = stride - width;  // 显示宽度与扫描线宽度的间隙  
+            IntPtr iptr = bmpData.Scan0;  // 获取bmpData的内存起始位置  
+            int scanBytes = stride * height;// 用stride宽度，表示这是内存区域的大小  
+
+            //// 下面把原始的显示大小字节数组转换为内存中实际存放的字节数组  
+            int posScan = 0, posReal = 0;// 分别设置两个位置指针，指向源数组和目标数组  
+            byte[] pixelValues = new byte[scanBytes];  //为目标数组分配内存  
+
+            for (int x = 0; x < height; x++)
+            {
+                //// 下面的循环节是模拟行扫描  
+                for (int y = 0; y < width; y++)
+                {
+                    pixelValues[posScan++] = rawValues[posReal++];
+                }
+                posScan += offset;  //行扫描结束，要将目标位置指针移过那段“间隙”  
+            }
+
+            //// 用Marshal的Copy方法，将刚才得到的内存字节数组复制到BitmapData中  
+            System.Runtime.InteropServices.Marshal.Copy(pixelValues, 0, iptr, scanBytes);
+            bmp.UnlockBits(bmpData);  // 解锁内存区域  
+
+            //// 下面的代码是为了修改生成位图的索引表，从伪彩修改为灰度  
+            ColorPalette tempPalette;
+            using (Bitmap tempBmp = new Bitmap(1, 1, PixelFormat.Format8bppIndexed))
+            {
+                tempPalette = tempBmp.Palette;
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                tempPalette.Entries[i] = Color.FromArgb(i, i, i);
+            }
+
+            bmp.Palette = tempPalette;
+
+            //// 算法到此结束，返回结果  
+            return bmp;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (myCamera != null)
+            {
+                myCamera.Close();
+                return;
+            }
+        }
+
+        private void gainTrackBar_Scroll(object sender, EventArgs e)
+        {
+            // Set Value
+            myGainNode.Value = gainTrackBar.Value;
+            // Update the Text Control with the new value
+            gainTextBox.Text = myGainNode.Value.ToString();
+        }
+
+        private void exposureTrackBar_Scroll(object sender, EventArgs e)
+        {
+            // Set Value
+            myExposureNode.Value = exposureTrackBar.Value;
+            // Update the Text Control with the new value
+            exposureTextBox.Text = myExposureNode.Value.ToString();
+        }  
+
+
+    }
+}
